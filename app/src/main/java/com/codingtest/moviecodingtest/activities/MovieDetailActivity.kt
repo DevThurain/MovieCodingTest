128d1d41db67d442ee3625756069ece1e9785187
thurain <thurainbowin.dev@gmail.com>
2023-07-24 00:28:04 +0630

    init project

---
+++ app/src/main/java/com/codingtest/moviecodingtest/activities/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.codingtest.moviecodingtest.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingtest.moviecodingtest.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
